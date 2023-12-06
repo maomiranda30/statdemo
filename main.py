906cac94014decb3c822c13f96c737273331f3e5
maomiranda30 <70478110+maomiranda30@users.noreply.github.com>
2023-12-06 11:53:45 -0500

    Update main.py

--- main.py
+++ main.py
@@ -243,7 +243,7 @@ def update_graph5(mao,slid):
         linea2=df[linea]    
         linea3=linea2.groupby(['AÑO','Nivel']).agg({mao:'count'}).reset_index()
         linea3['%'] = round(100 * linea3[mao] / linea3.groupby('AÑO')[mao].transform('sum'),0)
-        fig = px.line(linea3, x="AÑO", y='%', text='%',color='Nivel',range_x=[2017,2022.5],title='Puntaje por niveles')
+        fig = px.line(linea3, x="AÑO", y='%', text='%',color='Nivel',range_x=[2017,2023.5],title='Puntaje por niveles')
         fig.update_traces(textposition="bottom right")
         fig.update_layout(plot_bgcolor='rgba(0,0,0,0)',title=dict(font=dict(size=15)),title_x=0.5)  
 
@@ -265,7 +265,7 @@ def update_graph5(mao,slid):
         linea2=df[linea]    
         linea3=linea2.groupby(['AÑO','Nivel']).agg({mao:'count'}).reset_index()
         linea3['%'] = round(100 * linea3[mao] / linea3.groupby('AÑO')[mao].transform('sum'),0)
-        fig = px.line(linea3, x="AÑO", y='%', text='%',color='Nivel',range_x=[2017,2022.5],title='Puntaje por niveles')
+        fig = px.line(linea3, x="AÑO", y='%', text='%',color='Nivel',range_x=[2017,2023.5],title='Puntaje por niveles')
         fig.update_traces(textposition="bottom right")
         fig.update_layout(plot_bgcolor='rgba(0,0,0,0)',title=dict(font=dict(size=15)),title_x=0.5)  
         # fig.update_layout(width=534.5, height=440, autosize=False)
@@ -286,7 +286,7 @@ def update_graph5(mao,slid):
         linea2=df[linea]    
         linea3=linea2.groupby(['AÑO','Nivel']).agg({mao:'count'}).reset_index()
         linea3['%'] = round(100 * linea3[mao] / linea3.groupby('AÑO')[mao].transform('sum'),0)
-        fig = px.line(linea3, x="AÑO", y='%', text='%',color='Nivel',range_x=[2017,2022.5],title='Puntaje por niveles')
+        fig = px.line(linea3, x="AÑO", y='%', text='%',color='Nivel',range_x=[2017,2023.5],title='Puntaje por niveles')
         fig.update_traces(textposition="bottom right")
         fig.update_layout(plot_bgcolor='rgba(0,0,0,0)',title=dict(font=dict(size=15)),title_x=0.5)  
 
@@ -309,7 +309,7 @@ def update_graph5(mao,slid):
         linea2=df[linea]    
         linea3=linea2.groupby(['AÑO','Nivel']).agg({mao:'count'}).reset_index()
         linea3['%'] = round(100 * linea3[mao] / linea3.groupby('AÑO')[mao].transform('sum'),0)
-        fig = px.line(linea3, x="AÑO", y='%', text='%',color='Nivel',range_x=[2017,2022.5],title='Puntaje por niveles')
+        fig = px.line(linea3, x="AÑO", y='%', text='%',color='Nivel',range_x=[2017,2023.5],title='Puntaje por niveles')
         fig.update_traces(textposition="bottom right")
         fig.update_layout(plot_bgcolor='rgba(0,0,0,0)',title=dict(font=dict(size=15)),title_x=0.5)  
 
@@ -328,7 +328,7 @@ def update_graph5(mao,slid):
         linea2=df[linea]    
         linea3=linea2.groupby(['AÑO','Nivel']).agg({mao:'count'}).reset_index()
         linea3['%'] = round(100 * linea3[mao] / linea3.groupby('AÑO')[mao].transform('sum'),0)
-        fig = px.line(linea3, x="AÑO", y='%', text='%',color='Nivel',range_x=[2017,2022.5],title='Puntaje por niveles')
+        fig = px.line(linea3, x="AÑO", y='%', text='%',color='Nivel',range_x=[2017,2023.5],title='Puntaje por niveles')
         fig.update_traces(textposition="bottom right")
         fig.update_layout(plot_bgcolor='rgba(0,0,0,0)',title=dict(font=dict(size=15)),title_x=0.5)  
         return fig
